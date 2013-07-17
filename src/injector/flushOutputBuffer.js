aae92193a01d38a5a764ad7930b94f96dd585a67
rissem <rissem@gmail.com>
2013-07-17 12:24:03 -0700

    clarify weird debugger output flushing

--- src/injector/flushOutputBuffer.js
+++ src/injector/flushOutputBuffer.js
@@ -1,4 +1,6 @@
 var bufferSize = 80
+//pull the first 80 characters into madeyeResult
+//and return madeyeResult to the debugger
 __madeyeResult = __madeyeOutput__.slice(0,bufferSize);
 __madeyeOutput__ = __madeyeOutput__.slice(bufferSize);
 __madeyeResult;
