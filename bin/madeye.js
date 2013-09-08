53cd6b5b3e0af282745d6265c2e3aeeaff286249
James Gill <jamesagill@gmail.com>
2013-09-08 10:27:29 -0400

    Allow single base_host in madeye bin.
    Use it to determine other variables, if present.  If not, use the
    default variables.

--- bin/madeye.js
+++ bin/madeye.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
+
+if (process.env.MADEYE_BASE_HOST) {
+  //console.log("Using base host", process.env.MADEYE_BASE_HOST);
+
+  process.env.MADEYE_APOGEE_URL = "https://" + process.env.MADEYE_BASE_HOST
+  process.env.MADEYE_AZKABAN_URL = "https://api." + process.env.MADEYE_BASE_HOST
+  process.env.MADEYE_SOCKET_URL = "https://api." + process.env.MADEYE_BASE_HOST
+}
+
 require("coffee-script");
 require("../app").run();
