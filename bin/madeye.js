70dc230e82f1d7775e164703839b0142163089ec
rissem <rissem@gmail.com>
2013-09-09 22:27:09 -0700

    include full URL
    support http and https
    append api to azkaban url

--- bin/madeye.js
+++ bin/madeye.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-if (process.env.MADEYE_BASE_HOST) {
+if (process.env.MADEYE_BASE_URL) {
   //console.log("Using base host", process.env.MADEYE_BASE_HOST);
 
-  process.env.MADEYE_APOGEE_URL = "https://" + process.env.MADEYE_BASE_HOST
-  process.env.MADEYE_AZKABAN_URL = "https://api." + process.env.MADEYE_BASE_HOST
-  process.env.MADEYE_SOCKET_URL = "https://api." + process.env.MADEYE_BASE_HOST
+  process.env.MADEYE_APOGEE_URL = process.env.MADEYE_BASE_URL
+  process.env.MADEYE_AZKABAN_URL = process.env.MADEYE_BASE_URL + "/api";
+  process.env.MADEYE_SOCKET_URL = process.env.MADEYE_BASE_URL
 }
 
 require("coffee-script");
